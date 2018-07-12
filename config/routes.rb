2fdb50c0c7a36dcc939bac069dd3e9f4cb359528
Ben <benbaumann95@hotmail.co.uk>
2018-07-12 14:29:51 +0100

    Devise & Doorkeeper scaffold

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  use_doorkeeper do
+  	skip_controllers :authorizations, :applications,
+    	:authorized_applications
+	end
+
+  devise_for :users
+
+	root to: 'services#index'
+	
+  resources :services
+end
